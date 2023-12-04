a93e3f7c8d3e6b29e06f91a94bc887f817533803
hollowmatt <hollowmatt@gmail.com>
2023-12-04 02:21:32 +0000

    wire in the mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { NgFor } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgFor
   ],
   providers: [],
   bootstrap: [AppComponent]
