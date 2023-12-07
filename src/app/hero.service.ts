9519b4626729af060bc1c44782a93ba3e95d2212
hollowmatt <hollowmatt@gmail.com>
2023-12-07 19:42:39 +0000

    bind message service to hero and messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -9,10 +10,11 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes() : Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroServic: fetched heroes');
     return heroes;
   }
 }
