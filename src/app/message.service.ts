849e3474199eeed6ec278b953f95dbff21f6c6d7
hollowmatt <hollowmatt@gmail.com>
2023-12-07 19:38:46 +0000

    message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,7 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MessageService {
 
+export class MessageService {
+  messages: string[] = [];
   constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
