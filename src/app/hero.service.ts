d6c30620e1a4869ba5cd870d491b430cdb8dca64
hollowmatt <hollowmatt@gmail.com>
2023-12-11 01:33:40 +0000

    add delete method to api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -51,6 +51,14 @@ export class HeroService {
     );
   }
 
+  deleteHero(id: number): Observable<any> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
